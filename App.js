0a8a742b449cf91d66218971e907018884ac2096
ycurbill <108531077+ycurbill@users.noreply.github.com>
2023-11-11 22:27:06 +0100

    init tab navigator. Two options: Home or Favorites

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Home from './src/screens/Home';
+import Favorites from './src/screens/Favorites';
+import Fruit from './src/screens/Fruit';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Favorites" component={Favorites} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Favorites.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+const Favorites = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>Settings!</Text>
+        </View>
+    );
+}
+export default Favorites;
