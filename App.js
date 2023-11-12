eba1dc491fed14ba5b72e7334e42b73af192c157
ycurbill <108531077+ycurbill@users.noreply.github.com>
2023-11-12 16:24:18 +0100

    Styled Fruits List + Header and Tab Navigator

--- App.js
+++ App.js
@@ -1,23 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, NativeModules, Platform } from 'react-native';
+import IconFruit from 'react-native-vector-icons/MaterialCommunityIcons';
+import IconFavorite from 'react-native-vector-icons/Fontisto';
 
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Home from './src/screens/Home';
 import Favorites from './src/screens/Favorites';
 import Fruit from './src/screens/Fruit';
 
+const { StatusBarManager } = NativeModules;
+
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: 'rgba(222, 104, 130, 0.05)',
+  },
+};
+
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Favorites" component={Favorites} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={{ 
+      flex: 1, 
+      paddingTop: Platform.OS === 'android' ? StatusBarManager.HEIGHT : 0,
+    }}>
+      <NavigationContainer theme={MyTheme}>
+        <Tab.Navigator
+           screenOptions={({ route }) => ({
+            tabBarIcon: ({ color, size }) => {
+              if (route.name === 'Fruits List') {
+                return <IconFruit name='fruit-cherries' size={size} color={color} />;
+              } else if (route.name === 'Favorites') {
+                return <IconFavorite name='favorite' size={size} color={color} />;
+              }
+            },
+            tabBarActiveTintColor: 'rgb(111,35,177)',
+            tabBarInactiveTintColor: 'gray',
+            tabBarStyle: {
+              backgroundColor: 'rgba(222, 104, 130, 0.1)',
+              elevation: 0,
+            },
+            headerStyle: {
+              backgroundColor: 'rgba(222, 104, 130, 0.2)',
+            },
+            headerTintColor: 'white',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          })}
+        >
+          <Tab.Screen 
+            name="Fruits List" 
+            component={Home}
+          />
+          <Tab.Screen 
+            name="Favorites" 
+            component={Favorites} 
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,8 +1,9 @@
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, Pressable, StyleSheet } from "react-native";
 import { useEffect, useState } from "react";
 
 const Home = () => {
     const [fruits, setFruits] = useState();
+    const [timesPressed, setTimesPressed] = useState(0);
 
     useEffect(() => {
         fetch('https://www.fruityvice.com/api/fruit/all')
@@ -15,14 +16,51 @@ const Home = () => {
             });
     });
 
+    const onPressFunction = () => {
+        setTimesPressed(current => current + 1);
+    };
+
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', marginTop: 2 }}>
           <FlatList
             data={fruits}
-            renderItem={({item}) => <Text>{item.name}</Text>}
+            renderItem={({item}) => {
+                return (
+                    <Pressable 
+                        onPress={onPressFunction}
+                        style={({pressed}) => [
+                            {
+                                backgroundColor: pressed ? 'rgba(111,35,177,0.4)' : 'rgba(111,35,177,0.8)',
+                            },
+                            styles.wrapperCustom,
+                        ]}
+                    >
+                        <Text style={styles.text}>{item.name}</Text>
+                    </Pressable>
+                )
+            }}
+            showsVerticalScrollIndicator={false}
             keyExtractor={item => item.id}
           />
         </View>
     );
-}
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    text: {
+        color: 'white',
+        fontSize: 16,
+    },
+    wrapperCustom: {
+        borderRadius: 8,
+        padding: 6,
+        marginBottom: 6,
+        minWidth: 200,
+    },
+  });  
+
 export default Home;
